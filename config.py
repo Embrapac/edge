60346556a2c5a62ddbc56ff35cfbd628e7af9949
Manassés Souza <manasses.inatel@gmail.com>
2026-04-13 14:33:38 -0300

    #10 atualiza porta serial

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ class Config:
     STREAMER_URL = "http://10.7.202.108:8000/stream"
     BROKER_URL = "mqtt://localhost:1883"
     UART_ENABLED = True
-    UART_PORT = "/dev/serial0"
+    UART_PORT = "/dev/ttyAMA0"
     UART_BAUDRATE = 115200
     UART_TIMEOUT = 1.0
     MODELS_DIR = "models"
